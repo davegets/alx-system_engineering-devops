3894d4b9735b6db1aeda8ec9b9daa758c1e2089a
root <root@dave>
2024-02-06 19:25:29 +0300

    solved_from_errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,30 @@
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
 
-Terminal #1
-Terminal #fest to configure Nginx with a custom header
-class nginx_custom_header {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file_line { 'nginx_custom_header':
-    path => '/etc/nginx/sites-available/default',
-    line => '\tadd_header X-Served-By $hostname;',
-    match => '^\\tserver_name _;',
-    append_on_no_match => true,
-    notify => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-include nginx_custom_header
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
